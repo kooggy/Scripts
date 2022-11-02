e3854a1f5b95cffc2db6dc14391e7d4d82f142bf
sV <85319395+kooggy@users.noreply.github.com>
2022-11-02 18:23:07 -0400

    Create Find The Simpsons.lua

---
+++ Find The Simpsons.lua
@@ -0,0 +1,9 @@
+for i,v in next, game:GetService("StarterGui").ToutEstIci.Pedia.Pedi:GetChildren() do
+        if v.ClassName == "Frame" then
+            local args = {
+    [1] = v.Name
+}
+
+game:GetService("ReplicatedStorage").Nametag:FireServer(unpack(args))
+        end
+    end
